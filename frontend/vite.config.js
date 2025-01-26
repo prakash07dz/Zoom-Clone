23ea11b9556ceeddf1159538cac9f7af2ea646ce
Prakash Parmar <prakash07dz@gmail.com>
2025-01-26 09:56:00 +0530

    Updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split react and react-dom into a separate chunk
+        },
+      },
+    },
+    chunkSizeWarningLimit: 2000, // Set the chunk size limit to 2MB
+  },
+});
